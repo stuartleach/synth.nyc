b04292ef387108e631dc7bf541b420ae823e28ce
Stuart Leach <jstuartleach@gmail.com>
2023-04-25 10:53:46 -0600

    Fix sequencer stop/start functionality

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import * as Tone from 'tone';
 import {useSynthContext} from "./SynthContext";
 
-type Instrument = 'kick' | 'snare' | 'hihat';
+type Instrument = /*'kick' | 'snare' | 'hihat' |*/ 'synthKick' | 'synthSnare' | 'synthHihat';
 type Pattern = {
     [key in Instrument]: boolean[];
 };
@@ -21,28 +21,33 @@ export const Sequencer: React.FC = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentStep, setCurrentStep] = useState(0);
 
-    const drumSamples = {
-        kick: 'https://example.com/kick.wav',
-        snare: 'https://example.com/snare.wav',
-        hihat: 'https://example.com/hihat.wav',
-    };
+   /* const drumSamples = {
+        kick: "",
+        snare: "",
+        hihat: "",
+    };*/
 
     const synthDrums = {
-        kick: new Tone.MembraneSynth().toDestination(),
-        snare: new Tone.NoiseSynth().toDestination(),
-        hihat: new Tone.NoiseSynth().toDestination(),
+        synthKick: new Tone.MembraneSynth().toDestination(),
+        synthSnare: new Tone.NoiseSynth().toDestination(),
+        synthHihat: new Tone.NoiseSynth().toDestination(),
     }
 
-    const drumPlayers = new Tone.Players(drumSamples).toDestination();
+    // const drumPlayers = new Tone.Players(drumSamples).toDestination();
+
 
     const initialPattern: Pattern = {
-        kick: new Array(16).fill(false),
-        snare: new Array(16).fill(false),
-        hihat: new Array(16).fill(false),
+        /*kick: new Array(16).fill(false),*/
+        /*snare: new Array(16).fill(false),*/
+        /*hihat: new Array(16).fill(false),*/
+        synthKick: new Array(16).fill(false),
+        synthSnare: new Array(16).fill(false),
+        synthHihat: new Array(16).fill(false),
     };
     const [pattern, setPattern] = useState(initialPattern);
 
     const toggleStep = (instrument: Instrument, step: number) => {
+        console.log("toggleStep", instrument, step)
         setPattern((prevPattern) => {
             const newPattern = {...prevPattern};
             newPattern[instrument][step] = !newPattern[instrument][step];
@@ -51,40 +56,43 @@ export const Sequencer: React.FC = () => {
     };
 
 
-    useEffect(() => {
-
-        const sequence = new Tone.Sequence(
-            (time, step) => {
-                setCurrentStep(step);
-                for (const instrument in pattern) {
-                    const typedInstrument = instrument as Instrument;
-                    if (pattern[typedInstrument][step]) {
-                        drumPlayers.player(typedInstrument).start(time);
-                    }
+   useEffect(() => {
+    const sequence = new Tone.Sequence(
+        (time, step) => {
+            setCurrentStep(step);
+            for (const instrument in pattern) {
+                const typedInstrument = instrument as Instrument;
+                if (pattern[typedInstrument][step]) {
+                    // Replace drumPlayers with synthDrums and triggerAttackRelease instead of start
+                    synthDrums[typedInstrument].triggerAttackRelease("16n", time);
                 }
-            },
-            Array.from({length: 16}, (_, i) => i),
-            '16n'
-        );
+            }
+        },
+        Array.from({length: 16}, (_, i) => i),
+        '16n'
+    );
 
-        if (isPlaying) {
-            sequence.start(0);
-            Tone.Transport.start();
-        } else {
-            sequence.stop();
-            Tone.Transport.stop();
-        }
+    if (isPlaying) {
+        console.log("playing...")
+        sequence.start(0);
+        Tone.Transport.start();
+    } else {
+        console.log("stopped playing...")
+        sequence.stop();
+        Tone.Transport.stop();
+    }
 
-        return () => {
-            sequence.dispose();
-        };
-    }, [pattern, isPlaying]);
+    return () => {
+        sequence.dispose();
+    };
+}, [pattern, isPlaying]);
+
 
     return (
         <div className="sequencer">
             <button
                 className={`play-button ${isPlaying ? 'active' : ''}`}
-                onClick={() => setIsPlaying(!isPlaying)}
+                onClick={() => {Tone.Transport.start(); setIsPlaying(!isPlaying)}}
             >
                 {isPlaying ? 'Stop' : 'Play'}
             </button>
@@ -96,7 +104,7 @@ export const Sequencer: React.FC = () => {
                                 key={step}
                                 className={`step ${isActive ? 'active' : ''} ${
                                     currentStep === step ? 'current' : ''
-                                } bg-amber-300 p-6`}
+                                } bg-amber-300 p-3`}
                                 onClick={() => toggleStep(instrument as Instrument, step)}
                             >{isActive ? "🛢️" : "🥁"}</button>
                         ))}
